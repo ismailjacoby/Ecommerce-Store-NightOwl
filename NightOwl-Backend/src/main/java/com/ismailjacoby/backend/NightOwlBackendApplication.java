b770b77b3590457769ae24d2fa830d09eafa512e
Jacoby Ismail <ismail.jacoby@gmail.com>
2024-10-17 00:00:21 +0200

    Initial Commit for E-commerce Project

---
+++ NightOwl-Backend/src/main/java/com/ismailjacoby/backend/NightOwlBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ismailjacoby.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NightOwlBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NightOwlBackendApplication.class, args);
+	}
+
+}
