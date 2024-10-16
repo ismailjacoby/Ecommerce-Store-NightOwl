9a927e541427304ed435a0682e8b29afe011f21c
Jacoby Ismail <ismail.jacoby@gmail.com>
2024-10-17 01:32:42 +0200

    Added: Tailwind, DaisyUi and FontAwesome

--- NightOwl-Frontend/src/app/app.component.ts
+++ NightOwl-Frontend/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {FaConfig, FaIconLibrary} from '@fortawesome/angular-fontawesome';
+import {fontAwesomeIcons} from './shared/fonts/font-awesome-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'NightOwl-Frontend';
+export class AppComponent implements OnInit{
+  private faIconLibrary= inject(FaIconLibrary);
+  private faConfig= inject(FaConfig);
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    this.faConfig.defaultPrefix = 'far';
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
